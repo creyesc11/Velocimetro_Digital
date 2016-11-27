c0ab7e76db4b4162d875985ee1eae7385331fa02
Carlos Reyes <creyesc11@Carloss-Mac.local>
2016-11-27 12:21:42 -0800

    Initial Commit

---
+++ VelocimetroDig/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VelocimetroDig
+//
+//  Created by Carlos Reyes on 11/27/16.
+//  Copyright (c) 2016 Carlos Reyes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
